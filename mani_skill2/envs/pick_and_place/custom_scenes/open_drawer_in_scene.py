9f71b4a0761febcddf848309395d643147158724
Jiayuan-Gu <jigu@eng.ucsd.edu>
2024-01-12 18:47:22 -0800

    Add missing scene glb and update OpenDrawer env

--- mani_skill2/envs/pick_and_place/custom_scenes/open_drawer_in_scene.py
+++ mani_skill2/envs/pick_and_place/custom_scenes/open_drawer_in_scene.py
@@ -1,16 +1,19 @@
+"""
+python -m mani_skill2.examples.demo_manual_control \
+    -e OpenDrawerCustomInScene-v0 \
+    -c arm_pd_ee_delta_pose_align_interpolate_by_planner_gripper_pd_joint_target_delta_pos_interpolate_by_planner \
+    -o rgbd robot google_robot_static \
+    sim_freq @500 control_freq @15 scene_name frl_apartment_stage_simple scene_offset @[-1.8,-2.5,0.0]
+"""
 from collections import OrderedDict
 from typing import List, Optional
 
 import numpy as np
 import sapien.core as sapien
-from transforms3d.euler import euler2quat
-from transforms3d.quaternions import axangle2quat, qmult
-
-from mani_skill2.utils.common import random_choice
 from mani_skill2.utils.registration import register_env
-from mani_skill2.utils.sapien_utils import vectorize_pose
 
-from .base_env import CustomSceneEnv, CustomYCBInSceneEnv, CustomOtherObjectsInSceneEnv
+from .base_env import (CustomOtherObjectsInSceneEnv, CustomSceneEnv,
+                       CustomYCBInSceneEnv)
 
 
 class OpenDrawerInSceneEnv(CustomSceneEnv):
@@ -20,7 +23,7 @@ class OpenDrawerInSceneEnv(CustomSceneEnv):
     def _setup_lighting(self):
         # self.enable_shadow = True
         # super()._setup_lighting()
-        
+
         self._scene.set_ambient_light([0.3, 0.3, 0.3])
         # Only the first of directional lights can have shadow
         self._scene.add_directional_light(
@@ -29,7 +32,7 @@ class OpenDrawerInSceneEnv(CustomSceneEnv):
         self._scene.add_directional_light([-1, 0, 0], [0.5] * 3)
 
     def _load_actors(self):
-        self._load_actor_helper(add_collision=False)
+        self._load_arena_helper(add_collision=False)
 
     def _load_articulations(self):
         filename = str(self.asset_root / "mk_station.urdf")
@@ -37,7 +40,7 @@ class OpenDrawerInSceneEnv(CustomSceneEnv):
         loader.fix_root_link = True
         self._art_obj = loader.load(filename)
         # TODO: This pose can be tuned for different rendering approachs.
-        self._art_obj.set_pose(sapien.Pose([-0.5, 0, 0.0 + 0.017], [1, 0, 0, 0]))
+        self._art_obj.set_pose(sapien.Pose([-0.295, 0, 0.0 + 0.017], [1, 0, 0, 0]))
         for joint in self._art_obj.get_active_joints():
             # joint.set_friction(0.05)
             # joint.set_drive_property(stiffness=0, damping=1)
